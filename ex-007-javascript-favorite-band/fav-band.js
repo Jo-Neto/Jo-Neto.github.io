0b7d6e2811234140528cf5f6f30031394c5b913d
Jose Neto <jcb.oliveira.neto@gmail.com>
2021-11-19 16:23:34 -0300

    js fav band ex commit

---
+++ ex-007-javascript-favorite-band/fav-band.js
@@ -0,0 +1,25 @@
+document.querySelector('h1:first-of-type').innerHTML = "Author info";
+document.querySelector('section:last-of-type>h1').innerHTML = "Band info";
+
+document.querySelector('p:first-of-type').innerHTML = "Jose Neto";
+document.querySelector('p:nth-of-type(2)').innerHTML = "24 anos";
+document.querySelector('p:last-of-type').innerHTML = "Sao Paulo";
+document.querySelector('img:first-of-type').setAttribute("src", "images/fav-band.png");
+
+document.querySelector('section:last-of-type>p:first-of-type').innerHTML = "Band: Still Corners";
+document.querySelector('section:last-of-type>p:last-of-type').innerHTML = "Music Genre: Neo-Psychedelia/Dream Pop";
+document.querySelector('a:first-of-type').setAttribute("href", "https://www.youtube.com/channel/UCmubnYAiVP0Jmw6HhrK7uiQ");
+document.querySelector('a:first-of-type').innerHTML = "Conheça as musicas, vale muito a pena";
+document.querySelector('a:last-of-type').setAttribute("href", "https://en.wikipedia.org/wiki/Still_Corners");
+document.querySelector('a:last-of-type').innerHTML = "Conheça a historia";
+document.querySelector('div>img:nth-last-of-type(2)').setAttribute("src", "https://i.ytimg.com/vi/p50k_kklM7g/maxresdefault.jpg");
+document.querySelector('div>img:nth-last-of-type(1)').setAttribute("src", "https://i.ytimg.com/vi/IeAWn8zTbqg/maxresdefault.jpg");
+document.querySelector('ul>h2').innerHTML = "Melhores Musicas:";
+document.querySelector('li:first-of-type').innerHTML = "The Trip";
+document.querySelector('li:nth-of-type(2)').innerHTML = "Don't Fall in Love";
+document.querySelector('li:nth-of-type(3)').innerHTML = "Beggining to Blue";
+document.querySelector('li:nth-of-type(4)').innerHTML = "Lost Boys";
+document.querySelector('li:nth-of-type(5)').innerHTML = "The Last Exit";
+document.querySelector('li:nth-of-type(6)').innerHTML = "Static";
+document.querySelector('li:nth-of-type(7)').innerHTML = "The Message";
+document.querySelector('li:nth-of-type(8)').innerHTML = "White Sands";
